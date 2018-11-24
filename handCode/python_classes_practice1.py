585e6f7d075dac01421381d37256ff01605c9b4d
Jonathan <38092600+herrj1@users.noreply.github.com>
2018-11-23 22:18:58 -0500

    Added python classes implementation

---
+++ handCode/python_classes_practice1.py
@@ -0,0 +1,22 @@
+#python classes
+class Person:
+	def __init__(self,name,age):
+		self.name = name
+		self.age = age
+
+p1 = Person("Mark", 23)
+
+print(p1.name)
+print(p1.age)
+
+
+class kitchenTable
+	#rectangular shape W*L
+	def __init__(self, width, length)
+		self.width = width
+		self.length = length
+
+p2 = kitchenTable(23,34)
+
+print("The kitchen\'s table has a width of ", p2.width)
+print("The kitchen\'s table has a length of ", p2.length)
