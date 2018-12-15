e5b436ac93977fd806dc432e2f817ffeaf667cf5
Jonathan <38092600+herrj1@users.noreply.github.com>
2018-12-14 22:44:15 -0500

    Added app6 cnn implementation

---
+++ dataAnalytics/app6/CNN_dogs_and_cats.py
@@ -0,0 +1,43 @@
+import numpy as np
+import keras
+from keras import backend as k
+from keras.models import Sequential
+from keras.layers import Activation
+from keras.layers.core import Dense, Flatten
+from keras.optimizers import Adam
+from keras.metrics import categorical_crossentropy
+from keras.preprocessing.image importa ImageDataGenerator
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import *
+from matplotlib importa pyplot as plt
+from sklearn.metrics import confusion_matrix
+importa itertools
+import matplotlib.pyplot as plt
+%matplotlit inline
+
+train_path = 'cats-and-dogs/train'
+valid_path = 'cats-and-dogs/valid'
+test_path = 'cats-and-dogs/test'
+
+train_batches = ImageDataGenerator().flow_from_directory(train_path, target_size=(224,224), classes['dog', 'cat'], batch_size=10)
+valid_batches = ImageDataGenerator().flow_from_directory(valid_path, target_size=(224,224), classes['dog', 'cat'], batch_size=4)
+test_batches = ImageDataGenerator().flow_from_directory(test_path, target_size=(224,224), classes['dog', 'cat'], batch_size=10)
+
+#plot images with labesl in jUpyter notebook
+def plots(ims, figsize=(12,6), rows=1, interp=False, titles=None):
+	if type(ims[0]) is np.ndarray:
+		ims = np.array(ims).astype(np.uint8)
+		if(ims.shape[-1] != 3):
+			ims = ims.transpose((0,2,3,1))
+	f = plt.figure(figsize=figsize)
+	cols = len(ims) //rows if len(ims)%2 == 0 else len(ims)//rows + 1
+	for i in range(len(ims)):
+		sp = f.add_subplot(rows, cols, i+1)
+		sp.axis('Off')
+		if titles is not None:
+			sp.set_title(titles[i], fontsize=16)
+		plt.imshow(ims[i], interpolations=None if interp else 'none')
+
+imgs, labels = nex(train_batches)
+
+plots(imgs, titles=labels)
